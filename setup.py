4b2706e9a4a478123903ad64365cfd708431d911
term-extraction-project <156851259+term-extraction-project@users.noreply.github.com>
2024-07-21 19:33:36 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multi_word_expressions',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your project',
+    url='https://github.com/term-extraction-project/multi_word_expressions',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
